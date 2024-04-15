d43de0a4f0c03cbec3ce74684cc3a901e04105d8
SamPlayz6 <samddunning@hotmail.co.uk>
2024-04-15 16:40:39 +0100

    Completed the pretrained model code based on the Tiny Shakespeare dataset

--- bigram.py
+++ bigram.py
@@ -4,14 +4,18 @@ from torch.nn import functional as F
 
 
 #Hyper parameters
-batch_size = 32 # Amount of independant sequences to process in parrallel
-block_size = 8 # Maximum content length for a predictor. Never look at size bigger than block size when predicting
-max_iters = 3000
-eval_interval =  300
-learning_rate = 1e-2
+batch_size = 64 # Amount of independant sequences to process in parrallel
+block_size = 256 # Maximum content length for a predictor. Never look at size bigger than block size when predicting
+max_iters = 5000
+eval_interval =  500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters =200
-n_embed = 32 # Number of embedding dimensions
+eval_iters = 200
+n_embed = 384 # Number of embedding dimensions
+
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 #----------------------
 torch.manual_seed(1337) # Same randomly generated sequence of numbers
@@ -105,6 +109,88 @@ def estimate_loss():
     model.train() # Setting model to training phase - Some models behave differently in different modes. THats why it is important  
     return out
 
+
+class Head(nn.Module):
+    #One head of attention
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias = False)
+        self.query = nn.Linear(n_embed, head_size, bias = False)
+        self.value = nn.Linear(n_embed, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self,x):
+        B,T,C = x.shape
+        k = self.key(x)  #(B,T,C)
+        q = self.query(x)  #(B,T,C)
+
+        #Compute Attention Scores ("affinities") - Strength for that word to all the words preceeding it per word.
+        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B,T,T) @ (B,T,C) -> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) # (B,T,T)
+        wei = F.softmax(wei, dim = -1) # (B,T,T)
+        wei = self.dropout(wei)
+
+        #Perform the weighted aggregationg of the values
+        v = self.value(x)
+        out = wei @ v # (B,T,T) @ (B,T,C) -> (B,T,C)
+        return out
+
+
+class MulitHeadAttention(nn.Module):
+    # Multiple heads of self-attention in parallel
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim = -1) # Concatentate over the channel dimension
+        out = self.dropout(self.proj(out))
+        return out   
+
+
+
+class FeedForward(nn.Module):
+
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, 4 * n_embed),
+            nn.ReLU(),
+            nn.Linear(4 * n_embed, n_embed),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self,x):
+        return self.net(x)
+    
+
+
+class Block(nn.Module):
+    # Transformer block: communication followed by computation
+
+    def __init__(self, n_embed, n_head):
+        # n_embed: embedding dimension, n_head: The number of heads we'd like
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MulitHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed) # This normalizes your columns to a N(0,1) distribution
+        self.ln2 = nn.LayerNorm(n_embed) 
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+# Super simple bigram model
 class BigramLangageModel(nn.Module):
 
     def __init__(self):
@@ -112,6 +198,17 @@ class BigramLangageModel(nn.Module):
         #Each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed) 
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head= n_head) for _ in range(n_layer)])
+        # self.blocks = nn.Sequential(
+        #         Block(n_embed, n_head=4),
+        #         Block(n_embed, n_head=4),
+        #         Block(n_embed, n_head=4),
+        # )
+
+        self.ln_f = nn.LayerNorm(n_embed) 
+
+        # self.sa_head = MulitHeadAttention(4, n_embed//4) # 4 heads of quarter sel-attention size - This here is parralel convolutin 
+        # self.ffwd = FeedForward(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size )
 
     def forward(self,idx,targets = None):
@@ -119,8 +216,12 @@ class BigramLangageModel(nn.Module):
 
         #idx and tagets are both (B, T) tensors of integers
         tok_emb= self.token_embedding_table(idx) # (B, T, C) B - Batch, T - Time, C - Length of Batch?:  batch, time, channels
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)  torch.arrange() just is intergers from 0 to T
         x = tok_emb + pos_emb
+        # x = self.sa_head(x) # Applying Multiheaded Attention
+        # x = self.ffwd(x) # (B,T,C)
+        x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x) # (B,T,vocab_size) 
 
         if targets is None:
@@ -136,8 +237,11 @@ class BigramLangageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         #idx is (B,T) array of indices in the current context
         for _ in range(max_new_tokens):
+            #Crop idx to the last block_size
+            idx_cond = idx[:, -block_size:  ]
+
             #Getting predictions from forward function
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
 
             #focusing only on the last time step
             logits = logits[:,-1,:] # Become (B,C)
